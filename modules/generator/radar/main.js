dd5d2e0a70c30d935a5c63eec1adab9d74f9173a
Kurisu Brooks <kurisubrooks@gmail.com>
2017-01-13 01:19:56 +1100

    Add Adelaide, Static Image Generation

--- modules/generator/radar/main.js
+++ modules/generator/radar/main.js
@@ -4,26 +4,48 @@ const GIF = require("gifencoder");
 const Canvas = require("canvas");
 const qs = require("qs");
 
-let url = `http://data.weatherzone.com.au/json/animator/?` + qs.stringify({
-    "lt": "radar",
-    "lc": "071",
-    "type": "radar",
-    "mt": "radsat_640",
-    "mlt": "radarz",
-    "mlc": "071",
-    "frames": "8",
-    "md": "640x480",
-    "radardimensions": "640x480",
-    "df": "EEE HH:mm z",
-    "tz": "Australia/Sydney"
-}, { indices: false });
-
 module.exports = (server, data) => {
     let res = server.res;
     let request = server.modules.request;
     let path = server.modules.path;
     let fs = server.modules.fs;
     let _ = server.modules.lodash;
+
+    let frames = 8;
+    let list = { adelaide: "064", sydney: "071" };
+    let types = [ "animated", "static" ];
+    let type = data.type ? data.type : "static";
+    let id = data.id ? data.id : list.sydney;
+
+    if (data.type) {
+        if (!types[types.indexOf(data.type)]) {
+            res.status(400).send({ ok: false, code: 400, error: "unknown image type" });
+            return;
+        }
+    }
+
+    if (data.id) {
+        if (!list[data.id]) {
+            res.status(400).send({ ok: false, code: 400, error: "unknown/unsupported location" });
+            return;
+        } else {
+            id = list[data.id];
+        }
+    }
+
+    let url = `http://data.weatherzone.com.au/json/animator/?` + qs.stringify({
+        "lt": "radar",
+        "lc": id,
+        "type": "radar",
+        "mt": "radsat_640",
+        "mlt": "radarz",
+        "mlc": id,
+        "frames": frames,
+        "md": "640x480",
+        "radardimensions": "640x480",
+        "df": "EEE HH:mm z",
+        "tz": "Australia/Sydney"
+    }, { indices: false });
 
     request.get({ url: url, json: true }, (err, response, data) => {
         if (err) throw err;
@@ -36,58 +58,73 @@ module.exports = (server, data) => {
         const terrain = new Image();
         const locations = new Image();
 
-        terrain.src = fs.readFileSync(path.join(__dirname, "terrain.jpg"));
-        locations.src = fs.readFileSync(path.join(__dirname, "locations.png"));
+        terrain.src = fs.readFileSync(path.join(__dirname, "terrain", id + ".jpg"));
+        locations.src = fs.readFileSync(path.join(__dirname, "locations", id + ".png"));
 
-        encoder.start();
-        encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
-        encoder.setDelay(200);  // frame delay in ms
-        encoder.setQuality(100); // image quality. 10 is default.
+        if (type === "animated") {
+            encoder.start();
+            encoder.setRepeat(0);    // 0 for repeat, -1 for no-repeat
+            encoder.setDelay(220);   // frame delay in ms
+            encoder.setQuality(100); // image quality. 10 is default.
 
-        let count = 0;
-        let frames = { };
-        let requestQueue = [ ];
+            let count = 0;
+            let frames = { };
+            let requestQueue = [ ];
 
-        data.frames.sort((a, b) => a - b);
+            data.frames.sort((a, b) => a - b);
 
-        _.forEach(data.frames, (i) => {
-            let id = ++count;
+            _.forEach(data.frames, (i) => {
+                let id = ++count;
 
-            //console.log(`GET #${id} - ${i.timestamp_string}`);
+                //console.log(`GET #${id} - ${i.timestamp_string}`);
 
-            requestQueue.push(new Promise((resolve, reject) => {
-                request.get({ url: i.image, encoding: null }, (err, res, body) => {
-                    if (err) throw err;
+                requestQueue.push(new Promise((resolve, reject) => {
+                    request.get({ url: i.image, encoding: null }, (err, res, body) => {
+                        if (err) throw err;
 
-                    frames[id] = { radar: body, id: id, i: i };
-                    //console.log(`IMG #${id} - ${i.timestamp_string}`);
+                        frames[id] = { radar: body, id: id, i: i };
+                        //console.log(`IMG #${id} - ${i.timestamp_string}`);
 
-                    resolve();
+                        resolve();
+                    });
+                }));
+            });
+
+            Promise.all(requestQueue).then(() => {
+                //console.log(`Got ${requestQueue.length} frames total`);
+
+                _.forEach(frames, (thisFrame) => {
+                    frame.src = thisFrame.radar;
+
+                    ctx.drawImage(terrain, 0, 0);
+                    ctx.drawImage(frame, 0, 0);
+                    ctx.drawImage(locations, 0, 0);
+                    ctx.font = "16px sans-serif";
+                    ctx.fillText(thisFrame.id, 628, 15);
+
+                    //console.log(`ADD #${thisFrame.id} - ${thisFrame.i.timestamp_string}`);
+
+                    if (thisFrame.id === count) encoder.setDelay(2250);
+                    encoder.addFrame(ctx);
                 });
-            }));
-        });
 
-        Promise.all(requestQueue).then(() => {
-            //console.log(`Got ${requestQueue.length} frames total`);
+                encoder.finish();
+                res.type("gif");
+                res.end(encoder.out.getData());
+            });
+        } else {
+            request.get({ url: data.frames[Number(frames) - 1].image, encoding: null }, (err, resp, body) => {
+                if (err) throw err;
 
-            _.forEach(frames, (thisFrame) => {
-                frame.src = thisFrame.radar;
+                frame.src = body;
 
                 ctx.drawImage(terrain, 0, 0);
                 ctx.drawImage(frame, 0, 0);
                 ctx.drawImage(locations, 0, 0);
-                ctx.font = "16px sans-serif";
-                ctx.fillText(thisFrame.id, 628, 15);
 
-                //console.log(`ADD #${thisFrame.id} - ${thisFrame.i.timestamp_string}`);
-
-                if (thisFrame.id === count) encoder.setDelay(2250);
-                encoder.addFrame(ctx);
+                res.type("png");
+                res.send(canvas.toBuffer());
             });
-
-            encoder.finish();
-            res.type("gif");
-            res.end(encoder.out.getData());
-        });
+        }
     });
 };
